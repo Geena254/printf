b0ea8d3dd1c849d7687139d6c30363df437ce90d
Nic-Mwenda <mwendaniky805@gmail.com>
2023-07-25 19:09:33 -0700

    update

--- print_unsigned_number.c
+++ print_unsigned_number.c
@@ -5,14 +5,15 @@
  * @types: the list a of arguments
  * @buffer: The buffer array to handle print
  * @flags:  Calculates active flags
- * @width: Gets the width
+ * @wid: Gets the width
  * @precision: The precision specification
  * @size: The size specifier
  * Return: The number of chars printed.
  */
 
-int print_unsigned(va_list types, char buffer[]
-	int flags, int wid, int precision, int size)
+int print_unsigned(va_list types, char buffer[])
+
+	int flags, int wid, int precision, int size;
 {
 	int j = BUFF_SIZE - 2;
 	unsigned long int numb = va_arg(types, unsigned long int);
